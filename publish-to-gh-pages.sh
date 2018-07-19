25d67eeeab2544c79d6386cbb810b30bc0d86599
huangwj01 <huangwj01@mingyuanyun.com>
2018-07-19 15:47:16 +0800

    :tada: feat: 新增持续集成

---
+++ publish-to-gh-pages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+
+# get clone master
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+
+cd ../
+mv .deploy_git/.git/ ./public/
+
+cd ./public
+
+git config user.name "DogJun"
+git config user.email "498343874@qq.com"
+
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+
+# Github Pages
+git push --force --quiet "https://${Travis_Token}@${GH_REF}" master:master
